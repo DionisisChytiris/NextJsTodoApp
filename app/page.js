5068a174dfced63172dafca1d9039d960ce31359
Dionysios <dionysios8292@gmail.com>
2024-07-15 13:58:23 +0100

    test

--- app/page.js
+++ app/page.js
@@ -1,12 +1,47 @@
 "use client";
 import Todo from "@/components/Todo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 export default function Home() {
   const [formData, setFormData] = useState({
     title: "",
     description: "",
   });
+
+  const [todoData, setTodoData] = useState([]);
+
+  const fetchTodos = async () => {
+    const response = await axios("/api");
+    setTodoData(response.data.todos);
+  };
+
+  const deleteTodo = async(id)=>{
+
+    const response = await axios.delete('/api', {
+      params: {
+        mongoId: id
+      }
+    })
+    toast.success(response.data.msg)
+    fetchTodos()
+  }
+  
+  const completeTodo = async (id)=>{
+    const response = await axios.put('/api',{},{
+      params: {
+        mongoId: id
+      }
+    })
+    toast.success(response.data.msg)
+    fetchTodos()
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
   const onChangeHandler = (e) => {
     const name = e.target.name;
@@ -15,12 +50,29 @@ export default function Home() {
     console.log(formData);
   };
 
-  const onSubmitHandler = async (e)=>{
-    e.preventDefault()
-  }
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      //api code
+      const response = await axios.post("/api", formData);
+
+      toast.success(response.data.msg);
+      setFormData({
+        title: "",
+        description: "",
+      });
+      await fetchTodos()
+    } catch (error) {
+      toast.error("Error");
+    }
+  };
   return (
     <>
-      <form onSubmit={onSubmitHandler} className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+      <ToastContainer theme="dark" />
+      <form
+        onSubmit={onSubmitHandler}
+        className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto"
+      >
         <input
           value={formData.title}
           onChange={onChangeHandler}
@@ -63,9 +115,20 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            <Todo />
-            <Todo />
-            <Todo />
+            {todoData.map((item, index) => {
+              return (
+                <Todo
+                  key={index}
+                  id={index}
+                  title={item.title}
+                  description={item.description}
+                  complete={item.isCompleted}
+                  mongoId={item._id}
+                  deleteTodo={deleteTodo}
+                  completeTodo={completeTodo}
+                />
+              );
+            })}
           </tbody>
         </table>
       </div>
